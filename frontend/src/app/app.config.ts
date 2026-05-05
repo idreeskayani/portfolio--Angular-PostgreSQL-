5bc10482c4a84ed581d169e107ce3d40b5a4af7c
idreeskayani <Idreeskayani420@gmail.com>
2026-05-05 11:39:41 +0500

    <

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ROOT_ROUTES } from './navigator/root-navigation';
 import { authInterceptor } from './core/guards/auth.interceptor';
 
@@ -8,6 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(ROOT_ROUTES, withComponentInputBinding()),
-    provideHttpClient(withFetch(), withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
