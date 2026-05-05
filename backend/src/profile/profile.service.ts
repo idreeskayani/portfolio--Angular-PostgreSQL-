7c8f40aae8314519834c717a5ecdc37893d83ffa
idreeskayani <Idreeskayani420@gmail.com>
2026-05-05 12:18:39 +0500

    cors update

--- backend/src/profile/profile.service.ts
+++ backend/src/profile/profile.service.ts
@@ -14,10 +14,21 @@ export class ProfileService {
 
   async update(dto: UpdateProfileDto) {
     let profile = await this.repo.findOne({ where: { id: 1 } });
+    const sanitized = {
+      ...dto,
+      aboutParagraphs: dto.aboutParagraphs ?? [],
+      interests: dto.interests ?? [],
+      strengths: dto.strengths ?? [],
+      techTags: dto.techTags ?? [],
+      phone: dto.phone ?? '',
+      profilePic: dto.profilePic ?? '',
+      resumeUrl: dto.resumeUrl ?? '',
+      goalText: dto.goalText ?? '',
+    };
     if (!profile) {
-      profile = this.repo.create({ id: 1, ...dto });
+      profile = this.repo.create({ id: 1, ...sanitized });
     } else {
-      Object.assign(profile, dto);
+      Object.assign(profile, sanitized);
     }
     return this.repo.save(profile);
   }
